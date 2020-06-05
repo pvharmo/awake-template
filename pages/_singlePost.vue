c68982f2c0460c1e4042408cef85bea557f3eb9a
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2020-06-05 12:24:23 -0400

    Adjust pages content

--- pages/_singlePost.vue
+++ pages/_singlePost.vue
@@ -2,13 +2,6 @@
   <div id="post-page" class="page-wrapper post-page">
     <div class="post-wrapper">
       <markdown :markdown="$store.state.content" />
-      <div class="other-posts">
-        <h6 class="subtitle is-size-4">
-          Related Posts
-        </h6>
-        <!-- Related Posts -->
-        <posts-grid :number="3" :category="category" :exclude="slug" />
-      </div>
       <disqus-comments :identifier="$route.params.singlePost" />
     </div>
   </div>
