6d9ef917e2b8b310008be7a4db7f62ed9ec46548
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2020-06-13 15:41:51 -0400

    Remove site search

--- components/SiteNav.vue
+++ components/SiteNav.vue
@@ -22,19 +22,15 @@
             {{ item.name }}
           </component>
         </li>
-        <li class="navbar-item site-search-wrapper">
-          <site-search />
-        </li>
       </ul>
     </div>
   </nav>
 </template>
 <script>
-import SiteSearch from '~/components/SiteSearch'
 import HamburgerButton from '~/components/HamburgerButton'
 export default {
   name: 'SiteNav',
-  components: { SiteSearch, HamburgerButton },
+  components: { HamburgerButton },
   data() {
     return {
       active: false
